12a5b6a750bfc9e839e6a57500657eab638492ce
Andrew Vanderstarre <aavanderstarre@live.ca>
2020-08-13 00:59:08 -0400

    Set up library that controls LED Matrix

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
-#include <Arduino.h>
+#include "LedControl.h"
+
+/*
+    12 - DIN
+    11 - CLK
+    10 - CS
+*/
+
+// Defines led control
+LedControl lc=LedControl(12,11,10,1);
+
+// Delay time (ms)
+unsigned long delaytime=100;
+
+// Player variables
+uint8_t playerStartPos = 0b111;
 
 void setup() {
-  // put your setup code here, to run once:
+    // Wakes up MAX7219
+    lc.shutdown(0,false);
+    
+    // Sets brightness from 0 - 15
+    lc.setIntensity(0,0);
+
+    // Clears display 0
+    lc.clearDisplay(0);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() { 
+      lc.setRow(0, 5, playerStartPos);
 }
